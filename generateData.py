cf739896abd26da54b3646f744bdf05e3c05a21e
Cecilia Zhou <zilingzhou@college.harvard.edu>
2018-02-11 22:22:58 -0500

    changed to have no turkers, but still no solution

--- generateData.py
+++ generateData.py
@@ -11,14 +11,14 @@ import random
 
 
 
-N = 50 # total number of participants
-t = N * 0.4 # number of turkers
-Q = 4 # number of people per desired team
-Q_t = 1 # number of turkers per desired team
-M_1 = 2 # number of other types of skills (working together, fluid intelligence)
+N = 200 # total number of participants
+Q = 8 # number of people per desired team
+M_1 = 0 # number of other types of skills (working together, fluid intelligence)
 M_2 = 5 # number of topic specific skill dimensions
-M = M_1 + M_2 # total number of skills, used for diverse teams, topics only used for specific teams
-K_d = int(min(np.floor( t/2 / Q_t), np.floor((N-t)/2/ (Q-Q_t)))) # number of diverse teams
+M = M_1 + M_2# total number of skills, used for diverse teams, topics only used for specific teams
+W_d = int(N/2)
+K_d = int(np.floor(W_d/ Q)) # number of diverse teams
+print(K_d)
 K_s = None # number of specialized teams
 
 
@@ -36,8 +36,12 @@ K_s = None # number of specialized teams
 ## first we will generate data assuming skills are independent from one another
 ## then we will generate data increasing correlation between skills
 # M by N matrix, rows are skills
-V = np.random.normal(0,1, (M, int(N/2)))
-# assume that the first (N-t)/2 people are normal, and the last t/2 are turkers
+V = np.random.normal(0,1, (M, W_d))
+
+# add a dummy skill
+V = np.append(V, [[0]*W_d], axis = 0)
+M = M + 1
+
 
 # normalize the data if we are using real data, but in simulation, the standard deviation is already determined
 # for j in range(M):
@@ -48,7 +52,7 @@ V = np.random.normal(0,1, (M, int(N/2)))
 # determine the objective function, coefficients of the X_ijk which are just the V_ij's
 my_obj = np.array([V for _ in range(K_d)])
 my_obj = my_obj.flatten()
-num_vars = int(N/2 * K_d * M)
+num_vars = W_d * K_d * M
 my_ub = [1.0] * num_vars
 my_lb = [0.0] * num_vars
 assert(len(my_ub) == len(my_obj))
@@ -59,11 +63,14 @@ assert(len(my_ub) == len(my_obj))
 my_ctype = "I" * num_vars
 # NOTE: in the nonzero populate function we don't need the column or row name
 ## TODO RHS
-my_rhs = [1.0] * (int(N/2)  +  M * K_d) + [Q_t - Q] * K_d + [-Q_t] * K_d 
-my_sense = "E" * (int(N/2)) + "L" * (M* K_d + (2 * K_d))
+my_rhs = [1.0] * (int(N/2)  +  M * K_d) + [-Q] * K_d
+my_sense = "E" * (int(N/2)) + "L" * (M* K_d +  K_d)
 flatten = lambda l: [item for sublist in l for item in sublist]
+assert(len(my_rhs) == len(my_sense))
+print(len(my_sense))
 
 
+# introduce dummy skill 
 
 def populatebynonzero(prob):
     prob.objective.set_sense(prob.objective.sense.maximize)
@@ -72,29 +79,30 @@ def populatebynonzero(prob):
     prob.variables.add(obj=my_obj, lb=my_lb, ub=my_ub, types=my_ctype)
 
    #  rows = np.zeros((K_d, M, int(N/2)))
-    rows1 = [[i] * (M* K_d) for i in range(int(N/2))]
-    rows2 = [[int(N/2) + rownum] * (int(N/2)) for rownum in range(M* K_d)]
-    rows3 = [[int(N/2) + M* K_d + rownum]  * (int((N-t)/2) * M) for rownum in range(K_d)]
-    rows4 = [[int(N/2) + M* K_d + (K_d)] * (int(t/2) * M) for rownum in range(K_d)]
-    rows = [rows1, rows2, rows3, rows4]
+    rows1 = [[i] * (M* K_d) for i in range(W_d)]
+    rows2 = [[W_d + rownum] * (W_d) for rownum in range(M* K_d)]
+    rows3 = [[W_d + M* K_d + rownum]  * (W_d * M) for rownum in range(K_d)]
+   # rows4 = [[int(N/2) + M* K_d + (K_d)] * (int(t/2) * M) for rownum in range(K_d)]
+    rows = [rows1, rows2, rows3]
     rows = flatten(flatten(rows))
-    
+    # print(rows)
     cols1 = [[[int(N/2) * M * k + int(N/2) * j + i for j in range(M)] for k in range(K_d)] for i in range(int(N/2))]
     cols2 = [[[M * int(N/2) * k + int(N/2) * j + i for i in range(int(N/2))] for k in range(K_d)] for j in range(M)]
-    cols3 = [[[M * int(N/2) * k + int(N/2) * j + i for i in range(int((N-t)/2))] for j in range(M)] for k in range(K_d)] 
-    cols4 = [[[M * int(N/2) * k + int(N/2) * j + i for i in range(int(t/2))] for j in range(M)]for k in range(K_d)] 
-    cols = [cols1, cols2, cols3, cols4] 
+    cols3 = [[[M * int(N/2) * k + int(N/2) * j + i for i in range(W_d)] for j in range(M)] for k in range(K_d)] 
+    cols = [cols1, cols2, cols3] 
     cols = flatten(flatten(flatten(cols)))
-
     assert(len(cols) == len(rows))
 
-    vals = [1.0] * (int(N/2)  +  M * K_d) + [-1.0] * (2 * K_d)
+    vals = [1.0] * (W_d * M * K_d * 2) + [-1.0] * (W_d * M * K_d)
+    coeffs = zip(rows, cols, vals)
+    prob.linear_constraints.set_coefficients(coeffs)
+ 	
+    assert(len(rows) == len(vals))
 
-    prob.linear_constraints.set_coefficients(zip(rows, cols, vals))
 
 my_prob = cplex.Cplex()
-populatebynonzero(my_prob)
-# handle = 
+handle = populatebynonzero(my_prob)
+
 
 print("done generating")
 # exit(0)
